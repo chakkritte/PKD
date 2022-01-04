2291e832e07d0ca0d66a6b2691129a0b2c6929e7
Chakkrit Termritthikun <kongpasom@gmail.com>
2022-01-04 23:59:14 +0700

    Update test and add ensemble
    Update test and add ensemble

--- test.py
+++ test.py
@@ -33,18 +33,35 @@ output_size = (480, 640)
 readout = "simple"
 
 
-student = EEEAC2(num_channels=3, train_enc=True, load_weight=1, output_size=output_size, readout=readout)
-# student = EEEAC1(num_channels=3, train_enc=True, load_weight=1, output_size=output_size, readout=readout)
-# student = MobileNetV2(num_channels=3, train_enc=True, load_weight=1, output_size=output_size, readout=readout)
-# student = MobileNetV3_1k(num_channels=3, train_enc=True, load_weight=1, output_size=output_size, readout=readout)
-# student = EfficientNet(num_channels=3, train_enc=True, load_weight=1, output_size=output_size, readout=readout)
+#student = EEEAC2(num_channels=3, train_enc=True, load_weight=1, output_size=output_size, readout=readout)
+#student = EEEAC1(num_channels=3, train_enc=True, load_weight=1, output_size=output_size, readout=readout)
+#student = MobileNetV2(num_channels=3, train_enc=True, load_weight=1, output_size=output_size, readout=readout)
+#student = MobileNetV3_1k(num_channels=3, train_enc=True, load_weight=1, output_size=output_size, readout=readout)
+#student = EfficientNet(num_channels=3, train_enc=True, load_weight=1, output_size=output_size, readout=readout)
 # student = EfficientNetB4(num_channels=3, train_enc=True, load_weight=1, output_size=output_size, readout=readout)
 # student = EfficientNetB7(num_channels=3, train_enc=True, load_weight=1, output_size=output_size, readout=readout)
-# student = GhostNet(num_channels=3, train_enc=True, load_weight=1, output_size=output_size, readout=readout)
+#student = GhostNet(num_channels=3, train_enc=True, load_weight=1, output_size=output_size, readout=readout)
 # student = ResT(num_channels=3, train_enc=True, load_weight=1, output_size=output_size, readout=readout)
-
-computeTime(student)
+#student = OFA595(num_channels=3, train_enc=True, load_weight=1, output_size=output_size, readout=readout)
+student = VGGModel(num_channels=3, train_enc=True, load_weight=1, output_size=output_size)
 
 # x_image = Variable(torch.randn(1, 3, 384, 384))
 # y = model(x_image)
 
+#computeTime(student)
+
+# ### memory usage 
+from torch.profiler import profile, record_function, ProfilerActivity
+
+inputs = torch.randn(1, 3, 384, 384)
+
+with profile(activities=[ProfilerActivity.CPU],profile_memory=True, record_shapes=False) as prof:
+    student(inputs)
+
+print(prof.key_averages().table(sort_by="cpu_memory_usage", row_limit=10))
+
+### model size 
+# from torchsummary import summary
+# summary(student, input_size=(3, 384, 384))
+
+torch.save(student, "tmp.pt")
